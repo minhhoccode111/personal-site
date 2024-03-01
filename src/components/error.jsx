a887ce860afc4a8e4506bd9f694eaa6b86b9e212
minhhoccode111 <minhhoccode111@gmail.com>
2024-03-01 17:33:24 +0700

    add basic components

---
+++ src/components/error.jsx
@@ -0,0 +1,10 @@
+import { RiSignalWifiErrorFill } from 'react-icons/ri';
+
+export default function  Error  () {
+  return (
+    <h2 className="">
+      <RiSignalWifiErrorFill className="text-8xl" />
+    </h2>
+  );
+};
+
---
+++ src/components/loading.jsx
@@ -0,0 +1,10 @@
+import { AiOutlineLoading } from 'react-icons/ai';
+
+export default function Loading  () {
+  return (
+    <h2 className="animate-spin">
+      <AiOutlineLoading className="text-8xl" />
+    </h2>
+  );
+};
+
