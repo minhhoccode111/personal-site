73a5f408acd3ff211bb2082b38c92a45d264730b
minhhoccode111 <minhhoccode111@gmail.com>
2024-03-07 11:14:32 +0700

    add CustomButton component

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -12,7 +12,22 @@ export function SubmitButton({ children, isDisable }) {
   );
 }
 
+export function CustomButton({ isDisable, children, type = 'button', className = 'bg-link text-white' }) {
+  return (
+    <button disabled={isDisable} type={type} className={'inline-block rounded-lg px-5 py-3 text-sm font-medium transition-all hover:scale-110 hover:shadow hover:shadow-gray-400 ' + className}>
+      {children}
+    </button>
+  );
+}
+
 SubmitButton.propTypes = {
   isDisable: PropTypes.bool.isRequired,
   children: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+};
+
+CustomButton.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+  isDisable: PropTypes.bool.isRequired,
+  type: PropTypes.string,
+  className: PropTypes.string,
 };
