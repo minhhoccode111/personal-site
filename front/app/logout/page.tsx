16eab2a9cdcead9401df9d6d87f036f76e86964f
minhhoccode111 <minhhoccode111@gmail.com>
2024-06-26 21:12:55 +0700

    fix: bug Cannot update component when rendering a different component when logout

--- front/app/logout/page.tsx
+++ front/app/logout/page.tsx
@@ -1,24 +1,32 @@
 "use client";
 
 import { redirect } from "next/navigation";
-import Link from "next/link";
+// import Link from "next/link";
 
-import RouteHeader from "@/components/route-header";
+// import RouteHeader from "@/components/route-header";
 import useAuthStore from "@/stores/auth";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import RouteHeader from "@/components/route-header";
+import Link from "next/link";
 
 // extract the URL query then redirect to /about
 export default function Page() {
-  const setAuthData = useAuthStore((state) => state.setAuthData);
+  const { authData, setAuthData } = useAuthStore();
 
-  setAuthData({});
+  // run this reset authData first to logout
+  useEffect(() => {
+    setAuthData({});
+  }, [setAuthData]);
 
-  return redirect("/about");
+  /*
+   * NOTE: only redirect after the authData is reset mean the SiteHeader is
+   * already re-render, which help fix the annoy warning "Cannot update a
+   * component (`SiteHeader`) while rendering a different component (`Page`).
+   * */
+  useEffect(() => {
+    redirect("/about");
+  }, [authData]);
 
-  // return (
-  //   <div className="">
-  //     <RouteHeader>Logout success</RouteHeader>
-  //
-  //     <Link href={"/about"}>Go back</Link>
-  //   </div>
-  // );
+  return null;
 }
